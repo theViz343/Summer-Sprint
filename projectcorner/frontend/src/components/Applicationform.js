b675b4985fc030680ce5c6b49117bffd8ae4c816
theViz343 <vishwesh103@gmail.com>
2020-09-20 19:37:46 +0530

    Fixed application form frontend

--- projectcorner/frontend/src/components/Applicationform.js
+++ projectcorner/frontend/src/components/Applicationform.js
@@ -128,112 +128,42 @@ class Applicationform extends React.Component {
         <Navigationbar />
         <Header content="Enter Your Details"/>
         <div class="container p-3">
-        <div class="form-container">
 
 
-        <form method="post" name="editprof_form" action onsubmit="return validate_editprof_Form()">
+
+        <form method="post" name="application_form" onSubmit={this.handleSubmit}>
   <div className="container experience-content">
-    <div className="row" style={{padding: '0 10px'}}>
-      <div className="col-md-offset-2 col-md-8 col-xs-12 mid-content editprofile_form">
-        <div className="row">
-          <ul>
-            <li style={{color: 'red'}}>Error</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-md-6 col-xs-12 col-margin">
-            <label htmlFor="fname" className="control-label">First Name</label> <br />
-            <input type="text" id="fname" className="form-control" defaultValue="{{ person_obj.firstname }}" name="fname" />
-            <font color="red"> <div id="fnr_ep"> </div> </font>
-          </div>
-          <div className="col-md-6 col-xs-12 col-margin">
-            <label htmlFor="lname">Last Name</label> <br />
-            <input type="text" id="lname" className="form-control" defaultValue="{{ person_obj.lastname }}" name="lname" />
-            <font color="red"> <div id="lnr_ep"> </div> </font>
-          </div>
-        </div>
+    <div className="row justify-content-center">
+      <div className=" col-md-10 col-xs-12 mid-content editprofile_form">
         <br />
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-md-12 col-xs-12 col-margin">
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" className="form-control" readOnly defaultValue="{{ person_obj.email }}" name="email" />
+            <label htmlFor="statement_of_purpose">Your Statement of Purpose</label>
+            <textarea rows="5" type="text" required onChange={this.handleChange} className="form-control" value={this.state.value} name="statement_of_purpose" />
           </div>
         </div>
         <br />
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-md-6 col-xs-6 col-margin">
-            <label htmlFor="roll_no">Roll Number</label>
-            <input type="text" className="form-control" id="roll_no" style={{color: 'grey'}} readOnly defaultValue="{{ person_obj.roll_no }}" name="roll_no" />
+            <div class="form-group file-area">
+             <label htmlFor="resume">Upload Resume</label>
+            <input type="file" name="file-upload-field" id="resume" onChange = {this.handleResumeChange} required multiple="multiple"/>
+            <div class="file-dummy">
+              <div class="success">Great, your resume is selected. Keep on.</div>
+              <div class="default">Please select some files</div>
+            </div>
           </div>
-          <div className="col-md-6 col-xs-6 col-margin">
-            <label htmlFor="clg_id">College id</label>
-            <input type="text" className="form-control" style={{color: 'grey'}} readOnly defaultValue="{{ person_obj.clg_id }}" id="clg_id" name="clg_id" />
           </div>
         </div>
         <br />
-        <div className="row">
-          <div className="col-md-6 col-xs-6 col-margin">
-            <label htmlFor="telephone1">Mobile</label>
-            <input type="text" className="form-control" defaultValue="{{ person_obj.telephone1 }}" id="telephone1" name="telephone1" />
-            <font color="red"> <div id="mbr1_ep"> </div> </font>
-          </div>
-          <div className="col-md-6 col-xs-6 col-margin">
-            <label htmlFor="telephone2">Telephone</label>
-            <input type="text" className="form-control" defaultValue="{{ person_obj.telephone2 }}" id="telephone2" name="telephone2" />
-            <font color="red"> <div id="mbr2_ep"> </div> </font>
-          </div>
-        </div>
-        <div className="row editprofile_button_div">
-          <input type="button" onclick="display_password_change()" className=" editpass_button btn" defaultValue="Change Password " />
-          <div className="password_change" style={{display: 'none', marginTop: '10px'}}>
-            <div className="col-md-4 col-sm-6 col-xs-12">
-              <label htmlFor="oldpassword">Old Password</label>
-              <input type="password" className="form-control" defaultValue id="oldpasssword" name="oldpassword" />
-            </div>
-            <div className="col-md-4 col-sm-6 col-xs-12">
-              <label htmlFor="password1">New Password</label>
-              <input type="password" className="form-control" defaultValue id="passsword1" name="password1" />
-              <font color="red"> <div id="passr1_ep"> </div> </font>
-            </div>
-            <div className="col-md-4 col-sm-6 col-xs-12">
-              <label htmlFor="password2">Password again</label>
-              <input type="password" className="form-control" defaultValue id="passsword2" name="password2" />
-              <font color="red"> <div id="passr2_ep"> </div> </font>
-            </div>
-          </div>
-        </div>
-        <br />
-        <center><input type="submit" className="btn stupo-btn" name="submit" defaultValue="Update" /></center>
+        <center><input type="submit" className="btn stupo-btn" value="Submit Application" name="submit" /></center>
         <br /><br />
       </div>
     </div>
   </div>
 </form>
-
-
-
-
-            <form onSubmit={this.handleSubmit}>
-              Statement Of Purpose:
-              <input type="text" name="statement_of_purpose" value={this.state.value} onChange={this.handleChange} required/>
-              <br/>
-              <div class="file-upload-wrapper" data-text="Select your file!">
-                  <input name="file-upload-field" type="file" class="file-upload-field" value=""/>
-              </div>
-              <span id="filename">Upload Resume/CV</span>
-              <label for="file-upload">Browse
-              <input type="file" name="resume" id="resume" onChange = {this.handleResumeChange} required/>
-              </label>
-
-
-            <br/>
-            <br/>
-
-            <input class="btn btn-primary" value="Submit" type="submit"/>
-            </form>
             <br/>
             {message_alert()}
-        </div>
         </div>
         </div>
 
